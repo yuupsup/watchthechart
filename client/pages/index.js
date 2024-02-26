8c61987a66c79a872e4cf7b1c889d25ce4ff62cb
yuupsup <62122888+yuupsup@users.noreply.github.com>
2024-02-26 10:05:40 -0600

    Added dexscreener iframe

--- client/pages/index.js
+++ client/pages/index.js
@@ -8,13 +8,22 @@ export default function Home() {
         <a href="https://twitter.com/CHART_ON_SOLANA" target="_blank">Twitter</a>
       </div>
       <main className={styles.main}>
-        <section className={styles.infos}>
-          <h1>Utility</h1>
-          <p>$CHART is a token made to force you to look at the chart. The buybot will have no marketcap in Telegram.
-            The utility of the token is for you to learn how to buy and sell on your own conviction,
-            without hypemen or fudders in Telegram misleading you into false beliefs. Be a part of the movement. Watch the $CHART.
-          </p>
-        </section>
+        <div className={styles.section_container}>
+          <section className={styles.infos}>
+            <h1>Utility</h1>
+            <p>$CHART is a token made to force you to look at the chart. The buybot will have no marketcap in Telegram.
+              The utility of the token is for you to learn how to buy and sell on your own conviction,
+              without hypemen or fudders in Telegram misleading you into false beliefs. Be a part of the movement. Watch the $CHART.
+            </p>
+          </section>
+        </div>
+        <div className={styles.section_container}>
+          <section className={styles.charts}>
+            <div id="dexscreener-embed">
+              <iframe sandbox referrerPolicy={"no-referrer"} src="https://dexscreener.com/solana/2gNywbGr5hURypWAEk6UUXESx5cFmNrvajfbz81CEQzg?embed=1&amp;theme=dark"></iframe>
+            </div>
+          </section>
+        </div>
       </main>
     </div>
   );
