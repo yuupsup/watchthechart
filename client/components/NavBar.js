84ee834a2833b0d1b28c0b422d52b211c189e72f
yuupsup <62122888+yuupsup@users.noreply.github.com>
2024-02-29 13:58:26 -0600

    Updated UI

---
+++ client/components/NavBar.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import styles from '../styles/NavBar.module.css';
+
+import {Rubik} from 'next/font/google';
+const rubik = Rubik({
+  weight: '400',
+  subsets: ['latin']
+});
+
+export default function NavBar(props) {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [eventsActive, setEventsActive] = useState(false);
+
+  return (
+    <nav className={styles.navbar}>
+      <div className={`${styles.container}`}>
+        <a href="/" className={styles.logo}>
+          <img src="/logo-circle.svg" alt="Watch the Chart Logo"/>
+        </a>
+        <div className={`${styles.nav_links} ${rubik.className}`}>
+          <div className={`${styles.nav_item} ${styles.icon}`}>
+            <a href="https://twitter.com/CHART_ON_SOLANA">
+              <img src="/twitter.svg" alt="Watch the Chart - Twitter"/>
+            </a>
+          </div>
+          <div className={`${styles.nav_item} ${styles.icon}`}>
+            <a href="https://t.me/chartcommunitysol">
+              <img src="/telegram.svg" alt="Watch the Chart - Telegram"/>
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,30 +1,71 @@
 import styles from '../styles/Home.module.css';
+import NavBar from "@/components/NavBar";
+
+import {Rubik} from 'next/font/google';
+const rubik = Rubik({
+  weight: '400',
+  subsets: ['latin']
+});
+
 export default function Home() {
   return (
-    <div>
-      <div className={styles.header}>
-        <a href="https://t.me/chartcommunitysol" target="_blank">Telegram</a>
-        <img src="/chart2.png" alt="" id={styles.charting}/>
-        <a href="https://twitter.com/CHART_ON_SOLANA" target="_blank">Twitter</a>
+    <div className={styles.container}>
+      <NavBar />
+      <div className={styles.section_container}>
+        <section className={`${styles.infos} ${rubik.className}`}>
+          <h1>About</h1>
+          <p>$CHART is a token made to force you to look at the chart. The BuyBot will have no market cap in Telegram.
+            The utility of the token is to learn how to buy and sell on your own conviction,
+            without hype men or fudders in Telegram misleading you into false beliefs. Be a part of the movement. Watch the $CHART.
+          </p>
+        </section>
       </div>
-      <main className={styles.main}>
-        <div className={styles.section_container}>
-          <section className={styles.infos}>
-            <h1>Utility</h1>
-            <p>$CHART is a token made to force you to look at the chart. The buybot will have no marketcap in Telegram.
-              The utility of the token is for you to learn how to buy and sell on your own conviction,
-              without hypemen or fudders in Telegram misleading you into false beliefs. Be a part of the movement. Watch the $CHART.
-            </p>
-          </section>
-        </div>
-        <div className={styles.section_container}>
-          <section className={styles.charts}>
-            <div id="dexscreener-embed">
-              <iframe sandbox referrerPolicy={"no-referrer"} src="https://dexscreener.com/solana/2gNywbGr5hURypWAEk6UUXESx5cFmNrvajfbz81CEQzg?embed=1&amp;theme=dark"></iframe>
+      <div className={styles.section_container}>
+        <section className={`${styles.infos} ${rubik.className}`}>
+          <h1>Tokenomics</h1>
+          <div className={styles.pill_container}>
+            <div className={styles.pill}>
+              <div className={styles.header}>
+                Ticker
+              </div>
+              <div className={styles.detail}>
+                $CHART
+              </div>
             </div>
-          </section>
-        </div>
-      </main>
+            <div className={styles.pill}>
+              <div className={styles.header}>
+                Tax
+              </div>
+              <div className={styles.detail}>
+                0/0
+              </div>
+            </div>
+            <div className={styles.pill}>
+              <div className={styles.header}>
+                Burn
+              </div>
+              <div className={styles.detail}>
+                100%
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      <div className={styles.section_container}>
+        <section className={`${styles.infos} ${rubik.className}`}>
+          <h1>Contract Address</h1>
+          <p className={styles.ca}>
+            2gNywbGr5hURypWAEk6UUXESx5cFmNrvajfbz81CEQzg
+          </p>
+        </section>
+      </div>
+      <div className={styles.section_container}>
+        <section className={styles.charts}>
+          <div id="dexscreener-embed">
+            <iframe sandbox referrerPolicy={"no-referrer"} src="https://dexscreener.com/solana/2gNywbGr5hURypWAEk6UUXESx5cFmNrvajfbz81CEQzg?embed=1&amp;theme=dark"></iframe>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
